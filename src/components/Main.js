90d4a60bfda1a982ace392bbc8d4a21a5ba10b33
andresborn <andres.born@gmail.com>
2021-02-10 11:03:16 -0600

    Unnested components for better composition

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,53 @@
-import React from 'react'
-import Header from './Header'
-import List from './List'
-import Player from './Player'
+import React, { Component } from "react";
+import Header from "./Header";
+import List from "./List";
+import Player from "./Player";
+import Button from "./Button";
+export class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      songs: [],
+    };
+  }
 
-const Main = () => {
-    let songList;
+  componentWillMount() {
     const requestSongs = async () => {
-        const response = await fetch("https://assets.breatheco.de/apis/sound/songs");
-        const songs = await response.json()
-        songList = songs.map(song => `<li>${song.name}</li>`).join('')
-        console.log(songList)
-    }
-    requestSongs()
+      const response = await fetch(
+        "https://assets.breatheco.de/apis/sound/songs"
+      );
+      let songs = await response.json();
+      let songArray = [];
+      songs.forEach((song) => {
+        const songID = song.id;
+        const songName = song.name;
+        const songURL = song.url;
+        const songObj = { songID, songName, songURL };
+        songArray.push(songObj);
+      });
+      this.setState({ songs: songArray });
+    };
+    requestSongs();
+  }
 
+  render() {
     return (
-        <div className="container center column mx my">
-            <Header />
-            <List>
-                {}
-            </List>
-            <Player />
+      <div className="container center column mx my">
+        <Header />
+        <List>
+          {this.state.songs.map((song) => (
+            <li key={song.songID}>{song.songName}</li>
+          ))}
+        </List>
+        <Player />
+        <div className="center row mx">
+          <Button icon={<i className="bi-stop-fill icon"></i>} />
+          <Button icon={<i className="bi-play-fill icon"></i>} />
+          <Button icon={<i className="bi-skip-forward-fill icon"></i>} />
         </div>
-    )
+      </div>
+    );
+  }
 }
 
-export default Main
+export default Main;
--- src/components/Player.js
+++ src/components/Player.js
@@ -3,10 +3,9 @@ import Button from './Button.js'
 
 const Player = props => {
     return (
-        <div className="container center row mx">
-            <Button icon={<i className="bi-stop-fill icon"></i>} onClick={props.onClick} />
-            <Button icon={<i className="bi-play-fill icon"></i>} onClick={props.onClick} />
-            <Button icon={<i className="bi-skip-forward-fill icon"></i>} onClick={props.onClick} />
+        <div className="container center column mx">
+            <p>Audio</p>
+            <audio src={props.src}></audio>
         </div>
     )
 }
