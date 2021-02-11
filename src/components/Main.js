daa7634b93339335d53cbc5882436ee39e762d02
andresborn <andres.born@gmail.com>
2021-02-11 12:24:19 -0600

    Forwards and Backwards button func added

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,28 +10,29 @@ const Main = (props) => {
 
   const audioEL = useRef();
 
-  const requestSongs = async () => {
-    const response = await fetch(
-      "https://assets.breatheco.de/apis/sound/songs"
-    );
-    let songs = await response.json();
-    let songArray = [];
-    songs.forEach((song, i) => {
-      const songIdx = i;
-      const songName = song.name;
-      const songURL = song.url;
-      const songObj = { songIdx, songName, songURL };
-      songArray.push(songObj);
-    });
-    setSongs(songArray);
-  };
-  requestSongs();
+  useEffect(() => {
+    const requestSongs = async () => {
+      const response = await fetch(
+        "https://assets.breatheco.de/apis/sound/songs"
+      );
+      let songs = await response.json();
+      let songArray = [];
+      songs.forEach((song, i) => {
+        const songIdx = i;
+        const songName = song.name;
+        const songURL = song.url;
+        const songObj = { songIdx, songName, songURL };
+        songArray.push(songObj);
+      });
+      setSongs(songArray);
+    };
+    requestSongs();
+  }, []);
+
+  const apiURL = "https://assets.breatheco.de/apis/sound/";
 
   const changeSong = (i) => {
-    const music = songs[i].songURL;
-    const musicURL = "https://assets.breatheco.de/apis/sound/" + music;
-    console.log(musicURL);
-    setCurrentSong(musicURL);
+    setCurrentSong(songs[i]);
   };
 
   useEffect(() => {
@@ -40,26 +41,59 @@ const Main = (props) => {
   }, [currentSong]);
 
   const play = () => {
-          audioEL.current.play()
-          setIsPlaying(true)
-  }
-  
+    audioEL.current.play();
+    setIsPlaying(true);
+  };
+
   const pause = () => {
-        audioEL.current.pause()
-        setIsPlaying(false)
-  }
+    audioEL.current.pause();
+    setIsPlaying(false);
+  };
+
+  const forward = () => {
+    const next = songs.filter((song, i) => {
+      if (currentSong.songIdx === 21) {
+        return song.songIdx === 0;
+      } else {
+        if (song.songIdx === currentSong.songIdx + 1) {
+          return song;
+        }
+      }
+    });
+    setCurrentSong(next[0]);
+  };
+
+  const previous = () => {
+    const previous = songs.filter((song, i) => {
+      if (currentSong.songIdx === 0) {
+        return song.songIdx === 21;
+      } else {
+        if (song.songIdx === currentSong.songIdx - 1) {
+          return song;
+        }
+      }
+    });
+    setCurrentSong(previous[0]);
+  };
 
   const PlayButton = () => {
-      if (isPlaying) {
-          return <Button icon={<i className="bi-pause-fill icon"></i>} onClick={pause} />
-      } else {
-        return <Button icon={<i className="bi-play-fill icon"></i>} onClick={play} />
-      }
-  }
+    if (isPlaying) {
+      return (
+        <Button icon={<i className="bi-pause-fill icon"></i>} onClick={pause} />
+      );
+    } else {
+      return (
+        <Button icon={<i className="bi-play-fill icon"></i>} onClick={play} />
+      );
+    }
+  };
 
   return (
     <div className="container center column mx my">
-      <Header className="container center text-white" currentSong="Lets Play some music!" />
+      <Header
+        className="container center text-white"
+        currentSong="Lets Play some music!"
+      />
       <List className="text-white column">
         {songs.map((song, i) => (
           <li
@@ -75,11 +109,22 @@ const Main = (props) => {
         ))}
       </List>
       <div className="container center column mx">
-        <audio ref={audioEL} src={currentSong} type="audio/mp3"></audio>
+        <audio
+          ref={audioEL}
+          src={apiURL + currentSong.songURL}
+          type="audio/mp3"
+        ></audio>
       </div>
       <div className="center row mx">
+        <Button
+          icon={<i className="bi-skip-backward-fill icon"></i>}
+          onClick={previous}
+        />
         <PlayButton />
-        <Button icon={<i className="bi-skip-forward-fill icon"></i>} />
+        <Button
+          icon={<i className="bi-skip-forward-fill icon"></i>}
+          onClick={forward}
+        />
       </div>
     </div>
   );
